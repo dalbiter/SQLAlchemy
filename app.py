eed333ac634787e4a456af3d737e2bcc9a6213cf
danx <rushn24@gmail.com>
2023-08-29 17:11:40 -0400

    added pet list, and individual pet details routes

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///movies_example'
+app.app_context().push()
 
-db = SQLAlchemy()
-db.app = app
-db.init_app(app)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_db'
+# Below line may be necessary in older versions of SQLA, if you run in ipython and get warning...
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Below line is helpful while we are learning for debugging, SQLALCHEMY_ECHO to True
+app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'secret-key'
 app.config['DEBUG_TB_INTERECEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
+connect_db(app)
+
 @app.route('/')
 def show_home():
-    """Shows home page"""
-    return render_template('index.html')
+    """Shows list of all pets in db"""
+
+    pets = Pet.query.all()
+    return render_template('list.html', pets=pets)
+
+@app.route('/<int:pet_id>')
+def show_pet(pet_id):
+    """show details about a single pet"""
+
+    pet = Pet.query.get_or_404(pet_id)
+
+    return render_template('details.html', pet=pet)    
